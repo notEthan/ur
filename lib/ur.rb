d3e082be95dc4534867eed67e0916eeb060e6573
Ethan <ethan@unth>
2018-08-12 13:23:28 -0700

    Ur the initial with Ur::Request, Ur::Response, Ur::Processing

--- lib/ur.rb
+++ lib/ur.rb
@@ -1,5 +1,51 @@
 require "ur/version"
 
-module Ur
-  # Your code goes here...
+require 'jsi'
+
+Ur = JSI.class_for_schema({
+  id: 'https://schemas.ur.unth.net/ur',
+  type: 'object',
+  properties: {
+    bound: {
+      type: 'string',
+      description: %q([rfc2616] Inbound and outbound refer to the request and response paths for messages: "inbound" means "traveling toward the origin server", and "outbound" means "traveling toward the user agent"),
+      enum: ['inbound', 'outbound'],
+    },
+    request: {
+      type: 'object',
+      properties: {
+        method: {type: 'string', description: 'HTTP ', example: 'POST'},
+        uri: {type: 'string', example: 'https://example.com/foo?bar=baz'},
+        headers: {type: 'object'},
+        body: {type: 'string'},
+      },
+    },
+    response: {
+      type: 'object',
+      properties: {
+        status: {type: 'integer', example: 200},
+        headers: {type: 'object'},
+        body: {type: 'string'},
+      },
+    },
+    processing: {
+      type: 'object',
+      properties: {
+        began_at_s: {type: 'string'},
+        duration: {type: 'number'},
+        tags: {type: 'array', items: {type: 'string'}}
+      },
+    },
+  },
+})
+class Ur
+  Request = JSI.class_for_schema(self.schema['properties']['request'])
+  Response = JSI.class_for_schema(self.schema['properties']['response'])
+  Processing = JSI.class_for_schema(self.schema['properties']['processing'])
+  require 'ur/request'
+  require 'ur/response'
+  require 'ur/processing'
+
+  class << self
+  end
 end
---
+++ lib/ur/processing.rb
@@ -0,0 +1,6 @@
+require 'ur' unless Object.const_defined?(:Ur)
+
+class Ur
+  class Processing
+  end
+end

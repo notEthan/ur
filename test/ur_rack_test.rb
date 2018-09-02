e22e34759d9bb40d4c6f866e32b9bf48d900912c
Ethan <ethan@unth>
2018-09-02 15:17:12 -0700

    test Ur rack integration using Ur.from_rack_request

---
+++ test/ur_rack_test.rb
@@ -0,0 +1,29 @@
+require_relative 'test_helper'
+require 'rack'
+
+describe 'Ur rack integration' do
+  it 'builds from a rack env' do
+    env = Rack::MockRequest.env_for('https://ur.unth.net/', {'HTTP_FOO' => 'bar'})
+    ur = Ur.from_rack_request(env)
+    assert_equal('inbound', ur.bound)
+    assert_equal('GET', ur.request['method'])
+    assert_equal('bar', ur.request.headers['foo'])
+    assert_equal('https://ur.unth.net/', ur.request.uri)
+    assert(ur.response.empty?)
+    assert_instance_of(Time, ur.processing.began_at)
+    assert_nil(ur.processing.duration)
+    assert(ur.validate)
+  end
+  it 'builds from a rack request' do
+    env = Rack::Request.new(Rack::MockRequest.env_for('https://ur.unth.net/', {'HTTP_FOO' => 'bar'}))
+    ur = Ur.from_rack_request(env)
+    assert_equal('inbound', ur.bound)
+    assert_equal('GET', ur.request['method'])
+    assert_equal('bar', ur.request.headers['foo'])
+    assert_equal('https://ur.unth.net/', ur.request.uri)
+    assert(ur.response.empty?)
+    assert_instance_of(Time, ur.processing.began_at)
+    assert_nil(ur.processing.duration)
+    assert(ur.validate)
+  end
+end

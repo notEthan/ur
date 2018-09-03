ac940b3b658703cca3b36bb4df1ccc43067ae4b4
Ethan <ethan@unth>
2018-09-02 17:35:38 -0700

    test Ur faraday integration using Ur faraday middleware

---
+++ test/ur_faraday_test.rb
@@ -0,0 +1,62 @@
+require_relative 'test_helper'
+require 'faraday'
+require 'faraday_middleware'
+
+describe 'Ur faraday integration' do
+  it 'integrates, basic usage' do
+    ur = nil
+    faraday_conn = ::Faraday.new('https://ur.unth.net/') do |builder|
+      builder.use(Ur::FaradayMiddleware,
+        after_response: -> (ur_) { ur = ur_ },
+      )
+      builder.use(Faraday::Adapter::Rack, -> (env) { [200, {'Content-Type' => 'text/plain'}, ['ᚒ']] })
+    end
+    res = faraday_conn.get('/')
+    assert_equal('ᚒ', res.body)
+    assert_instance_of(Ur, ur)
+    assert_equal('get', ur.request['method'])
+    assert_equal('text/plain', ur.response.headers['Content-Type'])
+    assert_equal('ᚒ', ur.response.body)
+    assert(ur.validate)
+  end
+  it 'integrates, faraday middleware munges the json bodies but uses preserve_raw' do
+    ur = nil
+    faraday_conn = ::Faraday.new('https://ur.unth.net/') do |builder|
+      builder.request :json
+      builder.use(Ur::FaradayMiddleware,
+        after_response: -> (ur_) { ur = ur_ },
+      )
+      builder.response :json, preserve_raw: true
+      builder.use(Faraday::Adapter::Rack, -> (env) { [200, {'Content-Type' => 'application/json'}, ['{}']] })
+    end
+    res = faraday_conn.post('/', {'a' => 'b'})
+    assert_equal({}, res.body)
+    assert_instance_of(Ur, ur)
+    assert_equal('post', ur.request['method'])
+    assert_equal('{"a":"b"}', ur.request.body)
+    assert_equal('application/json', ur.response.headers['Content-Type'])
+    assert_equal('{}', ur.response.body)
+    assert(ur.validate)
+  end
+  it 'integrates, faraday middleware munges the json bodies and does not preserve_raw' do
+    ur = nil
+    faraday_conn = ::Faraday.new('https://ur.unth.net/') do |builder|
+      builder.use(Ur::FaradayMiddleware,
+        after_response: -> (ur_) { ur = ur_ },
+      )
+      builder.request :json
+      builder.response :json
+      builder.use(Faraday::Adapter::Rack, -> (env) { [200, {'Content-Type' => 'application/json'}, ['{}']] })
+    end
+    res = faraday_conn.post('/', {'a' => 'b'})
+    assert_equal({}, res.body)
+    assert_instance_of(Ur, ur)
+    assert_equal('post', ur.request['method'])
+    assert_nil(ur.request.body) # no good
+    assert_json_equal({"a" => "b"}, ur.request['body_parsed']) # best we get here
+    assert_equal('application/json', ur.response.headers['Content-Type'])
+    assert_nil(ur.response.body) # no good
+    assert_json_equal({}, ur.response['body_parsed']) # best we get here
+    assert(ur.validate)
+  end
+end
--- ur.gemspec
+++ ur.gemspec
@@ -32,6 +32,8 @@ Gem::Specification.new do |spec|
   spec.add_dependency "addressable", "~> 2.0"
   spec.add_development_dependency "rack"
   spec.add_development_dependency "rack-test"
+  spec.add_development_dependency "faraday"
+  spec.add_development_dependency "faraday_middleware"
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "minitest", "~> 5.0"

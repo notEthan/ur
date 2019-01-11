3c8e5e01b9917e808fe43642a88452a887c44ff8
Ethan <ethan@unth>
2019-01-11 15:32:58 -0800

    Ur::Response#success?, #client_error?, #server_error?

--- lib/ur/response.rb
+++ lib/ur/response.rb
@@ -3,5 +3,17 @@ require 'ur' unless Object.const_defined?(:Ur)
 class Ur
   class Response
     include RequestAndResponse
+
+    def success?
+      (200..299).include?(status)
+    end
+
+    def client_error?
+      (400..499).include?(status)
+    end
+
+    def server_error?
+      (500..599).include?(status)
+    end
   end
 end
